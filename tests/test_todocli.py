ae8b680f6aa133b99ee8f84a8b31fde477b5fb78
Dmani Barnett <dmani.barnett@gmail.com>
2022-10-12 15:33:33 +0100

    Added test for version shorthand option

--- tests/test_todocli.py
+++ tests/test_todocli.py
@@ -18,3 +18,10 @@ def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
+
+
+def test_version_short_opt():
+    """Tests app shows version."""
+    result = runner.invoke(cli.app, ["-v"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}\n" in result.stdout
