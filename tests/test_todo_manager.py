aa7b96d1405b3e86976a10bfa784423883766c98
Dmani Barnett <dmani.barnett@gmail.com>
2022-10-14 17:30:27 +0100

    Implemented adding todo

--- tests/test_todo_manager.py
+++ tests/test_todo_manager.py
@@ -1,8 +1,12 @@
 """Tests for `TodoManager` class."""
 
+import random
+
 import pytest
+from faker import Faker
 
 from todocli import todo_manager as tm
+from todocli.return_codes import Code
 
 
 @pytest.fixture
@@ -13,3 +17,20 @@ def todo_manager():
 
 def test_todo_manager_creation(todo_manager):
     assert isinstance(todo_manager, tm.TodoManager)
+
+
+def test_add_todo(todo_manager):
+    faker = Faker()
+    todo_task = faker.sentence()
+    todo_priority = random.randint(1, 3)
+    todo_due_date = faker.date_this_decade(after_today=True)
+    todo = todo_manager.add(
+        todo_task, todo_priority, todo_due_date.strftime("%Y-%m-%d")
+    )
+    return_todo = {
+        "Description": todo_task,
+        "Priority": todo_priority,
+        "Due": todo_due_date,
+        "Done": False,
+    }
+    assert todo == (return_todo, Code.SUCCESS)
--- todocli/todo_manager.py
+++ todocli/todo_manager.py
@@ -1,2 +1,14 @@
+from todocli.return_codes import Code
+
+
 class TodoManager:
-    pass
+    def add(self, description: str, priority: int, due: str):
+        return (
+            {
+                "Description": description,
+                "Priority": priority,
+                "Due": due,
+                "Done": False,
+            },
+            Code.SUCCESS,
+        )
