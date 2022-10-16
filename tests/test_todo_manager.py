c8faa83322ead2756ef40ca63d82ee04a1c42d14
Dmani Barnett <dmani.barnett@gmail.com>
2022-10-16 15:28:55 +0100

    Added test for multuple todos

--- tests/test_todo_manager.py
+++ tests/test_todo_manager.py
@@ -16,45 +16,67 @@ def todo_manager():
     yield todo_manager
 
 
+def generate_todos(num, include_due_date=True):
+    faker = Faker()
+    for _ in range(num):
+        todo_task = faker.sentence()
+        todo_priority = random.randint(1, 3)
+        todo_due_date = faker.date_this_decade(after_today=True)
+        todo_due_date_str = (
+            todo_due_date.strftime("%Y-%m-%d") if include_due_date else None
+        )
+        return_todo = {
+            "Description": todo_task,
+            "Priority": todo_priority,
+            "Due": todo_due_date_str,
+            "Done": False,
+        }
+        yield (todo_task, todo_priority, todo_due_date_str, return_todo)
+
+
 def test_todo_manager_creation(todo_manager):
     assert isinstance(todo_manager, tm.TodoManager)
 
 
-def test_add_todo(todo_manager):
-    faker = Faker()
-    todo_task = faker.sentence()
-    todo_priority = random.randint(1, 3)
-    todo_due_date = faker.date_this_decade(after_today=True)
-    todo_due_date_str = todo_due_date.strftime("%Y-%m-%d")
+@pytest.mark.parametrize(
+    "todo_task,todo_priority,todo_due_date_str,return_todo",
+    list(generate_todos(1)),
+)
+def test_add_todo(
+    todo_task, todo_priority, todo_due_date_str, return_todo, todo_manager
+):
     todo = todo_manager.add(todo_task, todo_priority, todo_due_date_str)
-    return_todo = {
-        "Description": todo_task,
-        "Priority": todo_priority,
-        "Due": todo_due_date_str,
-        "Done": False,
-    }
     assert todo == CurrentTodo(return_todo, Code.SUCCESS)
 
 
-def test_add_todo_returns_type(todo_manager):
-    faker = Faker()
-    todo_task = faker.sentence()
-    todo_priority = random.randint(1, 3)
-    todo_due_date = faker.date_this_decade(after_today=True)
-    todo_due_date_str = todo_due_date.strftime("%Y-%m-%d")
+@pytest.mark.parametrize(
+    "todo_task,todo_priority,todo_due_date_str,return_todo",
+    list(generate_todos(1)),
+)
+def test_add_todo_returns_type(
+    todo_task, todo_priority, todo_due_date_str, return_todo, todo_manager
+):
     todo = todo_manager.add(todo_task, todo_priority, todo_due_date_str)
     assert isinstance(todo, CurrentTodo)
 
 
-def test_add_todo_without_date(todo_manager):
-    faker = Faker()
-    todo_task = faker.sentence()
-    todo_priority = random.randint(1, 3)
-    todo = todo_manager.add(todo_task, todo_priority)
-    return_todo = {
-        "Description": todo_task,
-        "Priority": todo_priority,
-        "Due": None,
-        "Done": False,
-    }
+@pytest.mark.parametrize(
+    "todo_task,todo_priority,todo_due_date_str,return_todo",
+    list(generate_todos(1, False)),
+)
+def test_add_todo_without_date(
+    todo_task, todo_priority, todo_due_date_str, return_todo, todo_manager
+):
+    todo = todo_manager.add(todo_task, todo_priority, todo_due_date_str)
+    assert todo == CurrentTodo(return_todo, Code.SUCCESS)
+
+
+@pytest.mark.parametrize(
+    "todo_task,todo_priority,todo_due_date_str,return_todo",
+    list(generate_todos(10)),
+)
+def test_add_multiple_todo(
+    todo_task, todo_priority, todo_due_date_str, return_todo, todo_manager
+):
+    todo = todo_manager.add(todo_task, todo_priority, todo_due_date_str)
     assert todo == CurrentTodo(return_todo, Code.SUCCESS)
