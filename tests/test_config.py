b88181ab1d0240379f349e33378a3a419c4eebf5
Dmani Barnett <dmani.barnett@gmail.com>
2022-10-17 13:20:23 +0100

    Added test for config file db store

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import configparser
 from pathlib import Path
 
 from todocli import config
@@ -7,12 +8,24 @@ from todocli.return_codes import Code
 def test_config_file_created(tmp_path):
     config.CONFIG_DIR_PATH = tmp_path
     config.CONFIG_FILE_PATH = tmp_path / "config.ini"
-    assert config._init_config_file() == Code.SUCCESS
+    assert config._make_config_file() == Code.SUCCESS
     assert config.CONFIG_FILE_PATH.exists()
 
 
 def test_config_file_not_created():
     config.CONFIG_DIR_PATH = Path("/")
     config.CONFIG_FILE_PATH = Path("/") / "config.ini"
-    assert config._init_config_file() == Code.OS_ERROR
+    assert config._make_config_file() == Code.OS_ERROR
     assert config.CONFIG_FILE_PATH.exists() is False
+
+
+def test_config_file_file_db(tmp_path):
+    config.CONFIG_DIR_PATH = tmp_path
+    config.CONFIG_FILE_PATH = tmp_path / "config.ini"
+    db_path = str(tmp_path / "todo.json")
+    assert config._make_config_file() == Code.SUCCESS
+    assert config._init_config_file(db_path) == Code.SUCCESS
+    cfg = configparser.ConfigParser()
+    cfg.read(config.CONFIG_FILE_PATH)
+    assert "General" in cfg
+    assert cfg["General"] == {"database": db_path}
--- todocli/config.py
+++ todocli/config.py
@@ -1,3 +1,4 @@
+import configparser
 from pathlib import Path
 
 import typer
@@ -9,10 +10,21 @@ CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
 CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
 
 
-def _init_config_file():
+def _make_config_file():
     try:
         CONFIG_DIR_PATH.mkdir(exist_ok=True)
         CONFIG_FILE_PATH.touch(exist_ok=True)
+    except OSError:
+        return Code.OS_ERROR
+    return Code.SUCCESS
+
+
+def _init_config_file(db_path: str):
+    config_parser = configparser.ConfigParser()
+    config_parser["General"] = {"database": db_path}
+    try:
+        with CONFIG_FILE_PATH.open("w") as file:
+            config_parser.write(file)
     except OSError:
         return Code.OS_ERROR
     return Code.SUCCESS
