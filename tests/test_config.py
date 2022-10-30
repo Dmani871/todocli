e8cf430ec8d5b586060f7939614b29664ecb62c6
Dmani Barnett <dmani.barnett@gmail.com>
2022-10-30 15:27:52 +0000

    Added extras test without coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import configparser
 import json
 from pathlib import Path
+from unittest.mock import patch
 
 from todocli import config
 from todocli.return_codes import Code
@@ -50,6 +51,17 @@ def test_init_app(tmp_path):
     assert cfg["General"] == {"database": str(db_path)}
 
 
+@patch("todocli.config._make_config_file")
+def test_init_app_unsucessful_config_files_make(
+    mock_make_config_file, tmp_path
+):
+    mock_make_config_file.return_value = Code.OS_ERROR
+    config.CONFIG_DIR_PATH = tmp_path
+    config.CONFIG_FILE_PATH = tmp_path / "config.ini"
+    db_path = tmp_path / "todo.json"
+    assert config.init_app(db_path) == Code.OS_ERROR
+
+
 def test_read_db_path(tmp_path):
     config.CONFIG_DIR_PATH = tmp_path
     config.CONFIG_FILE_PATH = tmp_path / "config.ini"
@@ -67,3 +79,12 @@ def test_init_db(tmp_path):
     assert config.init_database(db_path) == Code.SUCCESS
     with open(str(db_path)) as db:
         assert json.load(db) == []
+
+
+def test_init_database_wo_init():
+    assert config.init_database(Path("/")) == Code.DB_INIT_ERROR
+
+
+def test_get_db_path_wo_init(tmp_path):
+    config.CONFIG_FILE_PATH = tmp_path / "config.ini"
+    assert config.get_db_path() == Code.CONFIG_READ_ERROR
